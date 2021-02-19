7b9f79df9a08879addd23e46a679bf751571f011
liuyong <543426720@qq.com>
2021-02-19 11:39:39 +0800

    修改文件

--- pages.config.js
+++ pages.config.js
@@ -4,7 +4,7 @@ module.exports={
   prdKey:'ciyun',//产品线名称
   cdn_console:[''], //日志查下
   cdn_common:[''],//公共cdn
-  api_statistics :'//ent-statis.love-health.com.cn/',//统计面板
+  api_statistics :'',//统计面板
   uploadUrl:'',//上传地址
   payHostUrl:''//支付地址
 }
